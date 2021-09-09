eaa2c53460cb8952407f57ee0031b5384ff6d061
Neha2109 <78674240+Neha2109@users.noreply.github.com>
2021-09-09 22:10:43 +0530

    Add files via upload

---
+++ Task-2/src/Components/Footer.js
@@ -0,0 +1,18 @@
+import React from "react"
+import { Container } from "reactstrap";
+
+const Footer = () => {
+    return (
+        <Container
+            fluid
+            tag="footer"
+            className="text-center bg-dark text-white fixed-bottom p-3"
+        >
+        
+        
+        Created by Neha
+        </Container>
+    )
+}
+
+export default Footer
